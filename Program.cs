cb5187ad1e695ff046e7d899527f89c836e94e66
George Gordon <georgegordon5@gmail.com>
2025-05-09 13:18:26 +0100

    most of it created just random weather to be added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace csharp_weather_app;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the amont of days you want to simulate"); //user intputs 7
+        int days = int.Parse(Console.ReadLine()); //so now days is is what the user input withch is 7 
+
+        int[] temperature = new int[days]; //creating a memory slot with 7 spaces called temperature 
+        string[] conditions = {"sunny", "rainy", "windy", "snowing", "icy"}; // an array with diffrent weather conditions
+        string[] weatherConditions = new string[days]; // crating a array called weatehr conditions which will eqaual new string[7] spaces of memory but is it now called new string?????
+
+    Random random = new Random(); // random number method? is it called a method?
+
+    for (int i = 0; i < days; i++) //itterating through a the 7 days 
+    {
+        temperature[i] = random.Next(-10, 39);   //chooseing a temperature between -10 and 38 
+            weatherConditions[i] = conditions[random.Next(conditions.Length)]; // so there is 7 spaces from before and it wil put rantdom weatehr conditions inside it 
+    }
+    }
+}
+ System.Console.WriteLine($"the weatehr today is  {weatherConditions}  {temperature}");
